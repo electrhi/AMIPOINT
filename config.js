856a540f4840fb45186536e68bdf8aa4d8a091e9
Hi <electrichi3882@gmail.com>
2026-06-09 09:28:31 +0900

    Connect app to Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WORKCHECK_CONFIG = {
   "SUPABASE_URL": "https://blbmdnygvoqyrovvlrrh.supabase.co",
-  "SUPABASE_PUBLISHABLE_KEY": ""
+  "SUPABASE_PUBLISHABLE_KEY": "sb_publishable_VmOfhSgbzTW-iNZNMdlUww_nfAO5C86"
 };
